f3bb502b0d7e179e9a9390d32c2ed11d016058ae
takuya kido <takuyakido1993@gmail.com>
2022-02-24 11:59:57 +0900

    AuthMiddlewareとAuthGuard追加

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { AuthGuard } from './shared/auth.guard';
 import { AuthService } from './shared/auth.service';
+import { TokenInterceptor } from './shared/token.interceptor';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -14,7 +17,11 @@ const routes: Routes = [
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   imports: [RouterModule.forChild(routes), CommonModule, FormsModule],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    AuthGuard,
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+  ],
   bootstrap: [],
 })
 export class AuthModule {}
---
+++ src/app/auth/shared/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGuard {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.authService.isAuthenticated()) return true;
+
+    alert('ログインしないと見れません');
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
